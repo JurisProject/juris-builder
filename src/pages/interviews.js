289cc9fde9be1e464a152325839191b332968e68
Konstantin Brazhnik <konst@ntin.solutions>
2020-06-22 13:10:41 -0500

    Fallback for interviews

--- src/pages/interviews.js
+++ src/pages/interviews.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRouteData } from 'react-static'
-import { Row, Col, Container, Card, CardBody } from 'reactstrap';
+import { Row, Col, Container, Card, CardBody, Alert } from 'reactstrap';
 import InterviewCard from '../components/InterviewCard/InterviewCard';
 
 const Interviews = () => {
@@ -9,9 +9,12 @@ const Interviews = () => {
     return (
         <Container className="pt-4 pb-4">
             <Row>
-                {interviews.map( i => <Col key={i.path} sm={12} md={6} lg={4}>
+                {interviews ? interviews.map( i => <Col key={i.path} sm={12} md={6} lg={4}>
                     <InterviewCard i={i} />
-                </Col>)}
+                </Col>) : <Alert>
+                    <h3>Sync your own interview library with GitHub!</h3>
+                    <p>We've made it super easy to sync a library to your own site.</p>
+                </Alert>}
             </Row>
         </Container>
     )
