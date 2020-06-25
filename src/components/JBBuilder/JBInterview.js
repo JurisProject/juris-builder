fd26aa071efefe0e5d7152ac699c27cfcfe3726a
Konstantin Brazhnik <konst@ntin.solutions>
2020-06-25 12:08:42 -0500

    AutoSave in LocalStorage

--- src/components/JBBuilder/JBInterview.js
+++ src/components/JBBuilder/JBInterview.js
@@ -7,8 +7,12 @@ import * as Survey from 'survey-react';
 import 'survey-react/survey.css';
 import 'survey-react/modern.css';
 
-const Interview = ({json, output, className, onUpdate, onComplete}) => {
+const Interview = ({json, output, className, onUpdate, onComplete, autoSave}) => {
     const [model, setModel] = useState(false);
+    const [initialData, setInitialData] = useState({});
+    const [initialPage, setInitialPage] = useState(0);
+    let storageName = 'JurisInterviewData'
+
     Survey.StylesManager.applyTheme("modern");
 
     useEffect(() => {
@@ -16,14 +20,47 @@ const Interview = ({json, output, className, onUpdate, onComplete}) => {
     },[]);
 
     function setupSurveyModel() {
+        if (autoSave || typeof autoSave === 'undefined') {
+            const autoSaveDataString = window.localStorage.getItem(storageName);
+            if (autoSaveDataString) {
+                const autoSaveData = JSON.parse(autoSaveDataString);
+                setInitialData(autoSaveData);
+
+                if (autoSaveData.pageNo) setInitialPage(autoSaveData.pageNo);
+            }
+        }
+
         setModel(new Survey.Model(json));
     }
 
-    const _onComplete = e => onComplete ? onComplete(e) : false;
+    function saveSurveyData(survey) {
+        let {data} = survey;
+        data.pageNo = survey.currentPageNo;
+        window.localStorage.setItem(storageName, JSON.stringify(data));
+      }
+
+    const _onComplete = e => {
+        window.localStorage.removeItem(storageName);
+
+        onComplete ? onComplete(e) : false;
+    }
     const _onUpdate = e => onUpdate ? onUpdate(e) : false;
 
+    const _onPartialSend = survey => {
+        console.log("Partial Send", {survey});
+        if (autoSave || typeof autoSave === 'undefined') saveSurveyData(survey);
+    }
+
     return (<div className={classNames('interview-container', className)}>
-        {model ? <Survey.Survey model={model} onComplete={_onComplete} onValueChanged={_onUpdate} /> : <Spinner />}
+        {model ? <Survey.Survey
+                model={model}
+                onComplete={_onComplete}
+                onValueChanged={_onUpdate}
+                sendResultOnPageNext={true}
+                onPartialSend={_onPartialSend}
+                data={initialData}
+                currentPageNo={initialPage}
+                /> : <Spinner />}
     </div>);
 }
 
