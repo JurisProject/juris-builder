bef5851e075faaa7e7bb106b8b2328804af5e056
Konstantin Brazhnik <konst@ntin.solutions>
2020-06-26 16:22:08 -0500

    Removed 404 flash

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
-export default () => (
-  <div>
-    <h1>404 - Oh no's! We couldn't find that page :(</h1>
-  </div>
-)
+export default () => {
+  const [ready, setReady] = React.useState(false)
+
+  React.useEffect(() => {
+    setReady(true)
+  }, [])
+
+  return ( ready ?
+    <div>
+      <h1>404 - Oh no's! We couldn't find that page :(</h1>
+    </div> : null
+  )
+}
